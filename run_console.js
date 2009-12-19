32bd06d0deb5c0ac428851b193bfdc21887f2b02
DracoBlue <JanS@DracoBlue.de>
2009-12-19 12:22:46 +0100

    Added run_console and run_server.

---
+++ run_console.js
@@ -0,0 +1,13 @@
+/*
+ * This file is part of the Spludo Framework.
+ * Copyright (c) 2009 DracoBlue, http://dracoblue.net/
+ *
+ * Licensed under the terms of MIT License. For the full copyright and license
+ * information, please see the LICENSE file in the root folder.
+ */
+
+require("./core");
+
+new ConsoleApplication({
+    "path": process.ARGV[2] || ""
+}).run();
