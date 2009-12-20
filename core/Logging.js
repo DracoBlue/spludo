ec81637f66b180fd2b9d2dc70298034e49f5a5ce
DracoBlue <JanS@DracoBlue.de>
2009-12-20 14:14:23 +0100

    Using the entire power for the configuration system and loading the logging only with those functions which are defined by logging.level.

--- core/Logging.js
+++ core/Logging.js
@@ -21,13 +21,81 @@ var logWithPrefix = function(prefix, args) {
     sys.debug(prefix + ': ' + message);
 
 }
+/*
+ * Log Levels:
+ *
+ *     6   <     5     <  4   <  3   <  2    <   1   <  0
+ *   TRACE < DEBUG/LOG < INFO < WARN < ERROR < FATAL < OFF
+ */
+
+var log_configuration = config.get(
+    "logging", {
+        "level": 3
+    }
+);
+
+var doNotLog = function() {}
 
 Logging = function() {};
-Logging.prototype = {
-    'log' : function() {
-        logWithPrefix("LOG ", arguments);
-    },
-    'info' : function() {
-        logWithPrefix("INFO", arguments);
+
+Logging.LEVEL_TRACE = 6;
+Logging.LEVEL_LOG = 5;
+Logging.LEVEL_DEBUG = 5;
+Logging.LEVEL_INFO = 4;
+Logging.LEVEL_WARN = 3;
+Logging.LEVEL_ERROR = 2;
+Logging.LEVEL_FATAL = 1;
+Logging.LEVEL_ALL = 0;
+
+if (log_configuration.level >= Logging.LEVEL_TRACE) {
+    Logging.prototype.trace = function() {
+        logWithPrefix("TRACE", arguments);
     }
+} else {
+    Logging.prototype.trace = doNotLog;
 }
+
+if (log_configuration.level >= Logging.LEVEL_DEBUG) {
+    Logging.prototype.log = function() {
+        logWithPrefix("LOG  ", arguments);
+    }
+    Logging.prototype.debug = function() {
+        logWithPrefix("DEBUG", arguments);
+    }
+} else {
+    Logging.prototype.debug = doNotLog;
+    Logging.prototype.log = doNotLog;
+}
+
+if (log_configuration.level >= Logging.LEVEL_INFO) {
+    Logging.prototype.info = function() {
+        logWithPrefix("INFO ", arguments);
+    }
+} else {
+    Logging.prototype.info = doNotLog;
+}
+
+if (log_configuration.level >= Logging.LEVEL_WARN) {
+    Logging.prototype.warn = function() {
+        logWithPrefix("WARN ", arguments);
+    }
+} else {
+    Logging.prototype.warn = doNotLog;
+}
+
+if (log_configuration.level >= Logging.LEVEL_ERROR) {
+    Logging.prototype.error = function() {
+        logWithPrefix("ERROR", arguments);
+    }
+} else {
+    Logging.prototype.error = doNotLog;
+}
+
+if (log_configuration.level >= Logging.LEVEL_FATAL) {
+    Logging.prototype.fatal = function() {
+        logWithPrefix("FATAL", arguments);
+    }
+} else {
+    Logging.prototype.fatal = doNotLog;
+}
+
