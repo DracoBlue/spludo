267459b96e3d4b493a7d0baa4e3ade718e434d51
DracoBlue <JanS@DracoBlue.de>
2009-12-20 21:27:05 +0100

    Added launcher for tests.

---
+++ build/run_tests.js
@@ -0,0 +1,13 @@
+/*
+ * This file is part of the Spludo Framework.
+ * Copyright (c) 2009 DracoBlue, http://dracoblue.net/
+ *
+ * Licensed under the terms of MIT License. For the full copyright and license
+ * information, please see the LICENSE file in the root folder.
+ */
+
+require("./../core");
+
+new TestApplication( {
+    "format": process.ARGV[2] || "console"
+}).run();
