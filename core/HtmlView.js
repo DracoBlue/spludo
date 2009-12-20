50ea086510d4df2a941d1f3374e08d422c785af5
DracoBlue <JanS@DracoBlue.de>
2009-12-20 17:38:16 +0100

    Moved from big object to prototype for initialization.

--- core/HtmlView.js
+++ core/HtmlView.js
@@ -13,66 +13,64 @@ HtmlView = function(name, content_file) {
 
 var html_view_format = {};
 
-HtmlView.prototype = {
-    "render": function(params, context) {
-        var content = null;
-        var posix = require("posix");
-        var sys = require("sys");
+HtmlView.prototype.render = function(params, context) {
+    var content = null;
+    var posix = require("posix");
+    var sys = require("sys");
 
-        var file_name = this.content_file;
+    var file_name = this.content_file;
 
-        if (typeof html_view_format[file_name] === "undefined") {
-            posix.cat(file_name).addCallback(function(file_content) {
-                content = file_content;
-            }).wait();
+    if (typeof html_view_format[file_name] === "undefined") {
+        posix.cat(file_name).addCallback(function(file_content) {
+            content = file_content;
+        }).wait();
 
-            var next_js_tag = content.indexOf("<?javascript");
-            var js_tag_length = "<?javascript".length;
+        var next_js_tag = content.indexOf("<?javascript");
+        var js_tag_length = "<?javascript".length;
 
-            if (next_js_tag === -1) {
-                html_view_format[file_name] = function() {
-                    return content;
-                };
-            } else {
-                /*
-                 * We have <?javascript ... ?> definitions, let's parse them!
-                 */
-                var body = [];
-                var current_block_start = 0;
-                var end_of_js_tag = 0;
-                var next_js_tag_end = 0;
+        if (next_js_tag === -1) {
+            html_view_format[file_name] = function() {
+                return content;
+            };
+        } else {
+            /*
+             * We have <?javascript ... ?> definitions, let's parse them!
+             */
+            var body = [];
+            var current_block_start = 0;
+            var end_of_js_tag = 0;
+            var next_js_tag_end = 0;
 
-                while (next_js_tag != -1) {
-                    next_js_tag_end = next_js_tag + js_tag_length;
-                    body.push("\ncontent.push(");
-                    body.push(JSON.stringify(content.substr(current_block_start, next_js_tag - current_block_start)));
-                    body.push(");\n");
+            while (next_js_tag != -1) {
+                next_js_tag_end = next_js_tag + js_tag_length;
+                body.push("\ncontent.push(");
+                body.push(JSON.stringify(content.substr(current_block_start, next_js_tag - current_block_start)));
+                body.push(");\n");
 
-                    end_of_js_tag = content.indexOf("?>", next_js_tag);
+                end_of_js_tag = content.indexOf("?>", next_js_tag);
 
-                    if (end_of_js_tag === -1) {
-                        throw new Error("<?javascript tag not finished!");
-                    }
-
-                    body.push("\n");
-                    body.push(content.substr(next_js_tag_end, end_of_js_tag - next_js_tag_end));
-                    body.push("\n");
-                    current_block_start = end_of_js_tag + 2;
-                    next_js_tag = content.indexOf("<?javascript", current_block_start);
+                if (end_of_js_tag === -1) {
+                    throw new Error("<?javascript tag not finished!");
                 }
 
-                if (current_block_start != content.length) {
-                    body.push("content.push(");
-                    body.push(JSON.stringify(content.substr(current_block_start, content.length)));
-                    body.push(");\n");
-                }
-                
-                var body_string = "var content = [];\n " + body.join("\n") + " \nreturn content.join(''); ";
-
-                html_view_format[file_name] = new Function("params", "context", body_string);
+                body.push("\n");
+                body.push(content.substr(next_js_tag_end, end_of_js_tag - next_js_tag_end));
+                body.push("\n");
+                current_block_start = end_of_js_tag + 2;
+                next_js_tag = content.indexOf("<?javascript", current_block_start);
             }
-        }
 
-        return html_view_format[file_name]();
+            if (current_block_start != content.length) {
+                body.push("content.push(");
+                body.push(JSON.stringify(content.substr(current_block_start, content.length)));
+                body.push(");\n");
+            }
+            
+            var body_string = "var content = [];\n " + body.join("\n") + " \nreturn content.join(''); ";
+
+            html_view_format[file_name] = new Function("params", "context", body_string);
+        }
     }
+
+    return html_view_format[file_name]();
 };
