5c8d231001cbc5d04a8009173ed59ee0aca31fda
DracoBlue <JanS@DracoBlue.de>
2009-12-20 17:56:05 +0100

    Moved the helper methods into a closure.

--- core/Logging.js
+++ core/Logging.js
@@ -31,74 +31,84 @@ Logging.LEVEL_ERROR = 2;
 Logging.LEVEL_FATAL = 1;
 Logging.LEVEL_OFF = 0;
 
-var log_configuration = config.get("logging", {
-    "level": Logging.LEVEL_WARN
-});
+(function() {
+    var log_configuration = config.get("logging", {
+        "level": Logging.LEVEL_WARN
+    });
 
-var logWithPrefix = function(prefix, args) {
-    var message = "null";
-    if (args.length > 1) {
-        message = sys.inspect(args);
+    /**
+     * Logs with a specific prefix. Should not be used directly at all!
+     * @private
+     */
+    var logWithPrefix = function(prefix, args) {
+        var message = "null";
+        if (args.length > 1) {
+            message = sys.inspect(args);
+        }
+    
+        if (args.length === 1) {
+            message = (typeof args[0] === "string") ? args[0] : sys.inspect(args[0]);
+        }
+    
+        sys.debug(prefix + ': ' + message);
+    };
+    
+    /**
+     * Empty function which is just available to allow calling a log method without effect!
+     * @private
+     */
+    var doNotLog = function() {
+    };
+    
+    if (log_configuration.level >= Logging.LEVEL_TRACE) {
+        Logging.prototype.trace = function() {
+            logWithPrefix("TRACE", arguments);
+        };
+    } else {
+        Logging.prototype.trace = doNotLog;
     }
-
-    if (args.length === 1) {
-        message = (typeof args[0] === "string") ? args[0] : sys.inspect(args[0]);
+    
+    if (log_configuration.level >= Logging.LEVEL_DEBUG) {
+        Logging.prototype.log = function() {
+            logWithPrefix("LOG  ", arguments);
+        };
+        Logging.prototype.debug = function() {
+            logWithPrefix("DEBUG", arguments);
+        };
+    } else {
+        Logging.prototype.debug = doNotLog;
+        Logging.prototype.log = doNotLog;
     }
-
-    sys.debug(prefix + ': ' + message);
-};
-
-var doNotLog = function() {
-};
-
-if (log_configuration.level >= Logging.LEVEL_TRACE) {
-    Logging.prototype.trace = function() {
-        logWithPrefix("TRACE", arguments);
-    };
-} else {
-    Logging.prototype.trace = doNotLog;
-}
-
-if (log_configuration.level >= Logging.LEVEL_DEBUG) {
-    Logging.prototype.log = function() {
-        logWithPrefix("LOG  ", arguments);
-    };
-    Logging.prototype.debug = function() {
-        logWithPrefix("DEBUG", arguments);
-    };
-} else {
-    Logging.prototype.debug = doNotLog;
-    Logging.prototype.log = doNotLog;
-}
-
-if (log_configuration.level >= Logging.LEVEL_INFO) {
-    Logging.prototype.info = function() {
-        logWithPrefix("INFO ", arguments);
-    };
-} else {
-    Logging.prototype.info = doNotLog;
-}
-
-if (log_configuration.level >= Logging.LEVEL_WARN) {
-    Logging.prototype.warn = function() {
-        logWithPrefix("WARN ", arguments);
-    };
-} else {
-    Logging.prototype.warn = doNotLog;
-}
-
-if (log_configuration.level >= Logging.LEVEL_ERROR) {
-    Logging.prototype.error = function() {
-        logWithPrefix("ERROR", arguments);
-    };
-} else {
-    Logging.prototype.error = doNotLog;
-}
-
-if (log_configuration.level >= Logging.LEVEL_FATAL) {
-    Logging.prototype.fatal = function() {
-        logWithPrefix("FATAL", arguments);
-    };
-} else {
-    Logging.prototype.fatal = doNotLog;
-}
+    
+    if (log_configuration.level >= Logging.LEVEL_INFO) {
+        Logging.prototype.info = function() {
+            logWithPrefix("INFO ", arguments);
+        };
+    } else {
+        Logging.prototype.info = doNotLog;
+    }
+    
+    if (log_configuration.level >= Logging.LEVEL_WARN) {
+        Logging.prototype.warn = function() {
+            logWithPrefix("WARN ", arguments);
+        };
+    } else {
+        Logging.prototype.warn = doNotLog;
+    }
+    
+    if (log_configuration.level >= Logging.LEVEL_ERROR) {
+        Logging.prototype.error = function() {
+            logWithPrefix("ERROR", arguments);
+        };
+    } else {
+        Logging.prototype.error = doNotLog;
+    }
+    
+    if (log_configuration.level >= Logging.LEVEL_FATAL) {
+        Logging.prototype.fatal = function() {
+            logWithPrefix("FATAL", arguments);
+        };
+    } else {
+        Logging.prototype.fatal = doNotLog;
+    }
+})();
