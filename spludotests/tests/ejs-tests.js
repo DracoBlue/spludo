e6b44e5f481b4607c77358d4db77d1171cc2ecff
DracoBlue <JanS@DracoBlue.de>
2009-12-22 22:43:20 +0100

    Added tests for exceptions on ejs.

--- spludotests/tests/ejs-tests.js
+++ spludotests/tests/ejs-tests.js
@@ -10,7 +10,7 @@ new TestSuite("PlainHtmlFiles", {
         var real_contents = "";
         posix.cat("testdata/plain_html_file.ejs").addCallback(function(contents) {
             real_contents = contents;        
-        });
+        }).wait();
 
         equal(new_view.render(), real_contents);
     },
@@ -26,7 +26,7 @@ new TestSuite("PlainHtmlFiles", {
         var real_contents = "";
         posix.cat("testdata/plain_html_file_with_beginning_tag.ejs").addCallback(function(contents) {
             real_contents = contents;        
-        });
+        }).wait();
 
         equal(new_view.render(), real_contents);
     }    
@@ -44,7 +44,7 @@ new TestSuite("SimpleEjsFiles", {
         var real_contents = "";
         posix.cat("testdata/ejs_example_with_indention_and_two_blocks.ejs.expected_output.txt").addCallback(function(contents) {
             real_contents = contents;        
-        });
+        }).wait();
         
         equal(new_view.render(), real_contents);
     },
@@ -59,8 +59,59 @@ new TestSuite("SimpleEjsFiles", {
         var real_contents = "";
         posix.cat("testdata/ejs_example_with_ejs_at_the_beginning.ejs.expected_output.txt").addCallback(function(contents) {
             real_contents = contents;        
-        });
+        }).wait();
         
         equal(new_view.render(), real_contents);
     }
+});
+
+new TestSuite("ExpressionEjsFiles", {
+    ejsFileWithMultipleExpressionBlocks: function() {
+        var new_view = new EjsView(
+            "plain_html_file",
+            "testdata/ejs_example_with_expression.ejs"
+        );
+
+        var posix = require("posix");
+
+        var real_contents = "";
+        posix.cat("testdata/ejs_example_with_expression.ejs.expected_output.txt").addCallback(function(contents) {
+            real_contents = contents;        
+        }).wait();
+        
+        equal(new_view.render(), real_contents);
+    },
+    
+    exceptionWithWrongBlock: function() {
+        var new_view = new EjsView(
+            "plain_html_file",
+            "testdata/ejs_exception_for_not_closed_expression_tag.ejs"
+        );
+        
+        try {
+            new_view.render()
+            fail("Should throw an exception!");
+        } catch (e) {
+            if (e.message.indexOf("tag not finished") === -1) {
+                fail("Should throw something about 'tag not finished', but threw a: " + e.message);
+            }
+        }
+    },
+    
+    exceptionForSemicolonInExpressionBlock: function() {
+        var new_view = new EjsView(
+            "plain_html_file",
+            "testdata/ejs_exception_for_semicolon_in_expression_tag.ejs"
+        );
+        
+        try {
+            new_view.render();
+            fail("Should throw an exception!");
+        } catch (e) {
+            if (e.message.indexOf("Syntax Error") === -1) {
+                fail("Should throw a Syntax Error, but threw a: " + e.message);
+            }
+        }
+    }
+    
 });
