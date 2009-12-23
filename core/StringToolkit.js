1d66d20a60689da7c621e7b0cac1f9b847ffcf52
DracoBlue <JanS@DracoBlue.de>
2009-12-23 21:37:49 +0100

    added jsdoc for the StringToolkit functions.

--- core/StringToolkit.js
+++ core/StringToolkit.js
@@ -34,12 +34,24 @@ var escaped_one_to_xml_special_map = {
  */
 StringToolkit = {
         
+    /**
+     * Escapes the 4 special entities for xml with the proper xml entities.
+     * 
+     * @param {String} string the input string
+     * @returns {String} The escaped xml string
+     */
     encodeXml: function(string) {
         return string.replace(/([\&"<>])/g, function(str, item) {
             return xml_special_to_escaped_one_map[item];
         });
     },
 
+    /**
+     * Unescapes the 4 special entities for xml.
+     * 
+     * @param {String} string the encoded input string
+     * @returns {String} The decoded xml string
+     */
     decodeXml: function(string) {
         return string.replace(/(&quot;|&lt;|&gt;|&amp;)/g, function(str, item) {
             return escaped_one_to_xml_special_map[item];
