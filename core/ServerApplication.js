c5ee25b16ca3bc844811bbfa015294da33d87a59
DracoBlue <JanS@DracoBlue.de>
2009-12-24 18:08:31 +0100

    Added basic post-data parser (not capable of multipart data, yet).

--- core/ServerApplication.js
+++ core/ServerApplication.js
@@ -43,7 +43,25 @@ ServerApplication.prototype.run = function() {
                 'Content-Type': 'text/plain'
             }
         };
+        var sys = require("sys");
 
+        if (body !== null) {
+            /*
+             * Ok, we need to parse that!
+             */
+             context.params = {};
+             var body_entries = body.split("&");
+             var body_entries_length = body_entries.length
+             var i = 0;
+
+             for (i=0; i<body_entries_length; i++) {
+                var row = body_entries[i].split("=");
+                var key = decodeURIComponent(row[0]);
+                var value = decodeURIComponent(row[1] || "");
+                context.params[key] = value;
+             }
+        }
+        
         ContextToolkit.applyRequestHeaders(context, req.headers);
 
         var response = null;
@@ -62,22 +80,25 @@ ServerApplication.prototype.run = function() {
         res.finish();
     };
     
-    this.server = http.createServer(function(req, res, body) {
-        if (req.method == "GET") {
-            finishRequest(req, res);
+    this.server = http.createServer(function(req, res) {
+        if (req.method === "GET") {
+            finishRequest(req, res, null);
+            return ;
         }
         
         var body = [];
-
-        req.addListener("body", function(content) {
-            if (content !== null) {
-                body.push(content);
-            }
-        });
         
-        req.addListener("complete",  function() {
-            finishRequest(req, res, body.join());
+        req.addListener('body', function(content) {
+            if (content === null) {
+                return ;
+            }
+            body.push(content);
         });
+
+        req.addListener('complete', function(content) {
+            finishRequest(req, res, body.join(""));
+        });
+
     });
 
     this.server.listen(this.options["port"]);
