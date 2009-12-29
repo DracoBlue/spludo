2acc2c06414fdfda1370493567a4f4412973d26d
DracoBlue <JanS@DracoBlue.de>
2009-12-29 14:14:40 +0100

    Added some generic mime type handling.

--- core/StaticFilesManager.js
+++ core/StaticFilesManager.js
@@ -49,27 +49,61 @@ StaticFilesManager.prototype.canHandleRequest = function(req) {
     return typeof this.files[uri] === "undefined" ? false : true;
 };
 
+var file_extension_to_mime_type_map = {
+        "js": "application/x-javascript",
+        "css": "text/css",
+        "jpg": "image/jpeg",
+        "jpeg": "image/jpeg",
+        "png": "image/png"
+}
+
 StaticFilesManager.prototype.handleRequest = function(req, res) {
     var uri = req.uri.full.substr(1);
+    
+    this.info('static: ' + uri);
+    var file_extension = /\.([\w]+)$/.exec(uri);
+
+    var mime_type = "application/octet-stream";
+    
+    if (file_extension) {
+        mime_type = file_extension_to_mime_type_map[file_extension[1]] || mime_type;
+    }
+    
     if (this.files[uri] === "undefined") {
-        throw new Error("Cannot handle the uri " + uri + "staticly.");
+        res.sendHeader(500, {
+            "Content-Type": "text/plain"
+        });
+        res.sendBody("cannot find static file " + uri);
+        res.finish();
     }
 
     var static_file_cache = {};
 
     var posix = require("posix");
 
-    var p = posix.cat(this.files[uri]);
-    
+    var p = posix.cat(this.files[uri], "binary");
+
     p.addCallback(function(content) {
-        res.sendHeader(200, {"Content-Type":"application/octet-stream"});
-        res.sendBody(content);
+        res.sendHeader(200, {
+            "Content-Type": mime_type,
+            "Cache-Control": "public, max-age=300"
+        });
+        res.sendBody(content, "binary");
         res.finish();
     });
-    
+
     p.addErrback(function() {
-        res.sendHeader(404, {});
+        res.sendHeader(404, {
+        });
         res.sendBody("");
         res.finish();
     });
+
+    p.addCancelback(function() {
+        res.sendHeader(404, {
+        });
+        res.sendBody("");
+        res.finish();
+    });
+
 };
