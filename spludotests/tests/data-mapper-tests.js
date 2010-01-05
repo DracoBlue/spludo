f1b1c3aac3217ac0e5e9f04f329b8e6ca0d730e5
DracoBlue <JanS@DracoBlue.de>
2010-01-05 16:33:49 +0100

    Added test for the json data mapper.

---
+++ spludotests/tests/data-mapper-tests.js
@@ -0,0 +1,29 @@
+/*
+ * This file is part of the Spludo Framework.
+ * Copyright (c) 2009 DracoBlue, http://dracoblue.net/
+ *
+ * Licensed under the terms of MIT License. For the full copyright and license
+ * information, please see the LICENSE file in the root folder.
+ */
+
+new TestSuite("data_mappers.JsonMapperTests", {
+
+    simpleEncodeDecode: function() {
+        var data_mapper = data_mapper_manager.getDataMapper('json');
+        
+        equal(data_mapper.encode("abcde"), "\"abcde\"");
+    },
+
+    expectingAnExceptionOnInvalidJson: function() {
+        var data_mapper = data_mapper_manager.getDataMapper('json');
+        
+        try {
+            data_mapper.decode("abcde");
+            fail("Should fail, because invalid json!");
+        } catch (e) {
+            notEqual(e.message.indexOf("Could not decode JSON"), -1, "Should throw something with 'Could not decode JSON'");
+        }
+    }
+
+
+});
