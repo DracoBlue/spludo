d258d86b42c69794ccd036185f89a7beb529505d
DracoBlue <JanS@DracoBlue.de>
2010-02-15 23:43:23 +0100

    happy new 2010!

--- core/Validator.js
+++ core/Validator.js
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Spludo Framework.
- * Copyright (c) 2009 DracoBlue, http://dracoblue.net/
+ * Copyright (c) 2009-2010 DracoBlue, http://dracoblue.net/
  *
  * Licensed under the terms of MIT License. For the full copyright and license
  * information, please see the LICENSE file in the root folder.
