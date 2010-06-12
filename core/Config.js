f2e0acadb57cdfc7072b420bd05e248667524df2
DracoBlue <JanS@DracoBlue.de>
2010-06-12 12:33:42 +0200

    Added setPathValue and getPathValue to config.

--- core/Config.js
+++ core/Config.js
@@ -28,3 +28,37 @@ Config.prototype.get = function(key, default_value) {
     }
     return this.values[key];
 };
+
+/**
+ * This method expands the configuration object as long as the path needs to be
+ * expanded to allow setting a property.
+ * 
+ * @param {Array}
+ *            path The path to apply the value to.
+ * @param {Object|String|Number|null}
+ *            value The value to apply to the path
+ * 
+ * @return {Object} The modified (or newly created) object
+ */
+Config.prototype.setPathValue = function(path, value) {
+    return ObjectToolkit.setPathValue(this.values, path, value);
+};
+
+/**
+ * This method does the opposite to Config#setPathValue.
+ * 
+ * @param {Array}
+ *            path The path to retrieve the value from.
+ * @param {Object|String|Number|null}
+ *            [value=undefined] The value to return in case the value is
+ *            undefined
+ * 
+ * @return {Object|String|Number|null} The value or undefined
+ */
+Config.prototype.getPathValue = function(path, default_value) {
+    var value = ObjectToolkit.getPathValue(this.values, path);
+    if (typeof value === "undefined") {
+        return default_value;
+    }
+    return value;
+};
