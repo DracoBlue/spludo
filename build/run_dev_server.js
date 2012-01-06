998db7376710cc43ec9ee7d61430fc86dce954b9
DracoBlue <JanS@DracoBlue.de>
2012-01-06 20:51:36 +0100

    ARGV is now process.argv. refs #5

--- build/run_dev_server.js
+++ build/run_dev_server.js
@@ -29,7 +29,7 @@ dev_server = {
         sys.debug('DEVSERVER: Starting server');
         that.watchFiles();
 
-        this.process = child_process.spawn(process.ARGV[0], ['run_server.js']);
+        this.process = child_process.spawn(process.argv[0], ['run_server.js']);
 
         this.process.stdout.addListener('data', function (data) {
             process.stdout.write(data);
